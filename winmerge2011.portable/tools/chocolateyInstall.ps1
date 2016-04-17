57f63a9b1e2ecf652e6a8abaeb7502dabe7564b7
Chris Charabaruk <chris.charabaruk@outlook.com>
2016-04-16 20:39:06 -0400

    set up winmerge2011 packages
    Currently, the winmerge2011.install package does not work, due to the
    fact that the installer is actually an Windows Script Host app without
    silent run ability. Until the installer can be run silently, only the
    winmerge2011.portable package will go up to the Chocolatey feed.

---
+++ winmerge2011.portable/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'winmerge2011.portable' # arbitrary name for the package, used in messages
+$url = '{{DownloadUrl}}' # download url
+$url64 = '{{DownloadUrlx64}}' # 64bit URL here or remove - if installer decides, then use $url
+
+try {
+  $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  Install-ChocolateyZipPackage "$packageName" "$url" "$installDir" "$url64"
+
+  # generate ignore and gui for executables
+  New-Item "$installDir\WinMergeU.exe.gui" -Type file -Force | Out-Null
+  New-Item "$installDir\tidy.exe.ignore" -Type file -Force | Out-Null
+
+  Write-ChocolateySuccess "$packageName"
+} catch {
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  throw
+}
---
+++ winmerge2011/tools/chocolateyInstall.ps1
@@ -0,0 +1,1 @@
+﻿#Install-VirtualPackage 'winmerge2011.portable' 'winmerge2011.install'
