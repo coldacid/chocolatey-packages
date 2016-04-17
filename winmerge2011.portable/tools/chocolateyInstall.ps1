97b8d9ee0a6e657e2c99317dab7301afca791917
Chris Charabaruk <chris.charabaruk@outlook.com>
2016-04-16 21:16:39 -0400

    winmerge2011: update based on automated package review

--- winmerge2011.portable/tools/chocolateyInstall.ps1
+++ winmerge2011.portable/tools/chocolateyInstall.ps1
@@ -2,16 +2,10 @@
 $url = '{{DownloadUrl}}' # download url
 $url64 = '{{DownloadUrlx64}}' # 64bit URL here or remove - if installer decides, then use $url
 
-try {
-  $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-  Install-ChocolateyZipPackage "$packageName" "$url" "$installDir" "$url64"
+$installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Install-ChocolateyZipPackage "$packageName" "$url" "$installDir" "$url64"
 
-  # generate ignore and gui for executables
-  New-Item "$installDir\WinMergeU.exe.gui" -Type file -Force | Out-Null
-  New-Item "$installDir\tidy.exe.ignore" -Type file -Force | Out-Null
-
-  Write-ChocolateySuccess "$packageName"
-} catch {
-  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
-  throw
-}
+# generate ignore and gui for executables
+New-Item "$installDir\WinMergeU.exe.gui" -Type file -Force | Out-Null
+New-Item "$installDir\tidy.exe.ignore" -Type file -Force | Out-Null
+New-Item "$installDir\Frhed\Frhed.exe.ignore" -Type file -Force | Out-Null
