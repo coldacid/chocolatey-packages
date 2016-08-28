048a0eb1004bd091502102e44509a0b1efa02df3
Chris Charabaruk <chris.charabaruk@outlook.com>
2016-08-28 15:21:43 -0400

    feat(winmerge2011): add download checksums

--- winmerge2011.install/tools/chocolateyInstall.ps1
+++ winmerge2011.install/tools/chocolateyInstall.ps1
@@ -12,6 +12,8 @@ $packageName = 'winmerge2011.install' # arbitrary name for the package, used in 
 $installerType = 'exe' #only one of these: exe, msi, msu
 $url = '{{DownloadUrl}}' # download url
 $url64 = '{{DownloadUrlx64}}'  # 64bit URL here or remove - if installer decides, then use $url
+$checksum = '{{Checksum}}'
+$checksum64 = '{{Checksumx64}}'
 $silentArgs = '/S' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
@@ -19,7 +21,9 @@ try {
   $installFile = Join-Path $env:TEMP "$packageName" "WinMerge2011_{{PackageVersion}}.exe"
 
   # downloader that the main helpers use to download items
-  Get-ChocolateyWebFile "$packageName" "$installFile" "$url" "$url64"
+  Get-ChocolateyWebFile "$packageName" -FileFullPath "$installFile" `
+                        -Url "$url" -Checksum "$checksum" -ChecksumType sha256 `
+                        -Url64 "$url64" -Checksum64 "$checksum64" -ChecksumType64 sha256
   # installer, will assert administrative rights - used by Install-ChocolateyPackage
   Install-ChocolateyInstallPackage "$packageName" "$installerType" "$silentArgs" "$installFile" -validExitCodes $validExitCodes
 
--- winmerge2011.portable/tools/chocolateyInstall.ps1
+++ winmerge2011.portable/tools/chocolateyInstall.ps1
@@ -1,9 +1,14 @@
 ﻿$packageName = 'winmerge2011.portable' # arbitrary name for the package, used in messages
 $url = '{{DownloadUrl}}' # download url
 $url64 = '{{DownloadUrlx64}}' # 64bit URL here or remove - if installer decides, then use $url
+$checksum = '{{Checksum}}'
+$checksum64 = '{{Checksumx64}}'
 
 $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-Install-ChocolateyZipPackage "$packageName" "$url" "$installDir" "$url64"
+Install-ChocolateyZipPackage "$packageName" `
+  -Url "$url" -Checksum "$checksum" -ChecksumType sha256 `
+  -Url64 "$url64" -Checksum64 "$checksum64" -ChecksumType64 sha256 `
+  -UnzipLocation "$installDir"
 
 # generate ignore and gui for executables
 New-Item "$installDir\WinMergeU.exe.gui" -Type file -Force | Out-Null
