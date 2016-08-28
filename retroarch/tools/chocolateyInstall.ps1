1620f1b3336e796fe2b1238590b29bb52f1efad0
Chris Charabaruk <chris.charabaruk@outlook.com>
2016-08-28 15:18:30 -0400

    feat(retroarch): add download checksum

--- retroarch/tools/chocolateyInstall.ps1
+++ retroarch/tools/chocolateyInstall.ps1
@@ -2,11 +2,16 @@
 
 $packageName = 'retroarch' # arbitrary name for the package, used in messages
 $url = '{{DownloadUrl}}' # download url
-$url64 = $url.Replace('/x86/', '/x86_64/') # 64bit URL here or remove - if installer decides, then use $url
+$url64 ='{{DownloadUrlx64}}' # 64bit URL here or remove - if installer decides, then use $url
+$checksum = '{{Checksum}}'
+$checksum64 = '{{Checksumx64}}'
 
 # if removing $url64, please remove from here
 $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-Install-ChocolateyZipPackage "$packageName" "$url" "$installDir" "$url64"
+Install-ChocolateyZipPackage "$packageName" `
+  -Url "$url" -Checksum "$checksum" -ChecksumType sha256 `
+  -Url64 "$url64" -Checksum64 "$checksum64" -ChecksumType64 sha256 `
+  -UnzipLocation "$installDir"
 
 New-Item "$installDir\retroarch.exe.gui" -Type file -Force | Out-Null
 New-Item "$installDir\retroarch_debug.exe.gui" -Type file -Force | Out-Null
