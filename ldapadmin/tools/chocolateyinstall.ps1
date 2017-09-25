8f1731fd6fab75963f22203a1c84a06ede31b7b3
Chris Charabaruk <chris.charabaruk@outlook.com>
2017-09-25 17:40:52 -0400

    ldapadmin: add package

---
+++ ldapadmin/tools/chocolateyinstall.ps1
@@ -0,0 +1,30 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$packageName= 'ldapadmin'
+$version    = '{{PackageVersion}}'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = '{{DownloadUrl}}'
+$url64      = '{{DownloadUrlx64}}'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+
+  url           = $url
+  url64bit      = $url64
+
+  checksum      = '{{Checksum}}'
+  checksumType  = 'sha256'
+  checksum64    = '{{Checksumx64}}'
+  checksumType64= 'sha256'
+
+  options       = @{
+    Headers     = @{
+      'User-Agent' = 'Wget/1.19.1'
+    }
+  }
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+New-Item (Join-Path $toolsDir "LdapAdmin.exe.gui") -Type file -Force | Out-Null
