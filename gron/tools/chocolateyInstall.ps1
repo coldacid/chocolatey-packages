7f89dbd398c93bed6b638e3336e79ee21dd1cd65
Chris Charabaruk <chris.charabaruk@outlook.com>
2018-04-10 21:08:35 -0400

    gron: update to 0.5.2

--- gron/tools/chocolateyInstall.ps1
+++ gron/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName= 'gron'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$zip32 = 'gron-windows-386-0.5.1.zip'
-$zip64 = 'gron-windows-amd64-0.5.1.zip'
+$zip32 = 'gron-windows-386-0.5.2.zip'
+$zip64 = 'gron-windows-amd64-0.5.2.zip'
 
 $packageArgs = @{
   packageName    = $packageName
