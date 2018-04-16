ebf88898ed4fff45679ee4a1e9507e59a5069460
Chris Charabaruk <chris.charabaruk@outlook.com>
2018-04-15 22:25:13 -0400

    chore(pgadmin4): Convert to AU for updates

--- pgadmin4/tools/chocolateyInstall.ps1
+++ pgadmin4/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
-$ErrorActionPreference = 'Stop'; # stop on all errors
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 
 $packageName = 'pgadmin4'
-$version     = "{{PackageVersion}}"
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url         = '{{DownloadUrl}}' # download url
-$checksum    = "{{Checksum}}"
+$url         = 'https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v3.0/windows/pgadmin4-3.0-x86.exe' # download url
+$checksum    = '392d2386b8235d7ef5ef8e58ba262c26e2452d8cbb56e5d3a827017f72860d66'
+$checksumType= 'sha256'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -17,7 +17,7 @@ $packageArgs = @{
 
   softwareName  = 'pgAdmin 4'
   checksum      = $checksum
-  checksumType  = 'sha256'
+  checksumType  = $checksumType
 }
 
 Install-ChocolateyPackage @packageArgs
---
+++ pgadmin4/update.ps1
@@ -0,0 +1,34 @@
+Import-Module au
+. $PSScriptRoot\..\_scripts\all.ps1
+
+$releases = 'https://www.pgadmin.org/download/pgadmin-4-windows/'
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyinstall.ps1" = @{
+      "(?i)(^\s*[$]packageName\s*=\s*)('.*')" = "`$1'$($Latest.PackageName)'"
+      "(?i)(^\s*[$]url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+      "(?i)(^\s*[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+      "(?i)(^\s*[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $regex   = '(?:/pgadmin/pgadmin4/v)(\d+(\.\d+)+)(?:/windows)'
+  $url     = $download_page.links | Where-Object href -Match $regex | Select-Object -First 1 -Expand href
+  $version = $url -replace ".*v(\d+(\.\d+)*).*",'$1'
+
+  Write-Host $url $version
+
+  $url32 = "https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v${version}/windows/pgadmin4-${version}-x86.exe"
+
+  return @{
+    Version = $version
+
+    URL32 = $url32
+  }
+}
+
+Update-Package
