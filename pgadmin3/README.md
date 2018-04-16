6649c5ada3176d54562d105342626cae1e25603a
Chris Charabaruk <chris.charabaruk@outlook.com>
2018-04-15 22:11:25 -0400

    chore(pgadmin3): Convert to AU for updates

---
+++ pgadmin3/README.md
@@ -0,0 +1,7 @@
+# pgAdmin 3
+
+**OBSOLETE:** This program has been replaced with pgAdmin 4 and is no longer supported by its developers. You can install pgAdmin 4 via Chocolatey: `choco install pgadmin4`
+
+pgAdmin is the most popular and feature rich Open Source administration and development platform for PostgreSQL, the most advanced Open Source database in the world.
+
+pgAdmin is designed to answer the needs of all users, from writing simple SQL queries to developing complex databases. The graphical interface supports all PostgreSQL features and makes administration easy. The application also includes a syntax highlighting SQL editor, a server-side code editor, an SQL/batch/shell job scheduling agent, support for the Slony-I replication engine and much more. Server connection may be made using TCP/IP or Unix Domain Sockets (on *nix platforms), and may be SSL encrypted for security. No additional drivers are required to communicate with the database server.
--- pgadmin3/tools/chocolateyInstall.ps1
+++ pgadmin3/tools/chocolateyInstall.ps1
@@ -1,14 +1,13 @@
-$packageName    = 'pgadmin3'
+﻿$packageName    = 'pgadmin3'
 $installerType  = 'msi'
 $silentArgs     = "/quiet"
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
-$version = "{{PackageVersion}}"
-
-$zip_file = "pgadmin3-${version}.zip"
-$zip_url  = "{{DownloadUrl}}"
-$msi_file = "pgadmin3.msi"
-$checksum = "{{Checksum}}"
+$zip_file     = 'pgadmin3-1.22.2.zip'
+$zip_url      = 'https://ftp.postgresql.org/pub/pgadmin/pgadmin3/v1.22.2/win32/pgadmin3-1.22.2.zip'
+$msi_file     = 'pgadmin3.msi'
+$checksum     = 'dead649ddac52518d01976dffc0d85dd626f874c7b1c542eb5c817a2a6cd634d'
+$checksumType = 'sha256'
 
 
 $chocTempDir   = Join-Path $env:TEMP "chocolatey"
@@ -36,9 +35,8 @@ Write-Host "  * Zip File Temp: $zip_file_temp"
 Write-Host "  * Zip URL......: $zip_url"
 Write-Host ""
 # No special 64-bit builds available
-Get-ChocolateyWebFile "$packageName" $zip_file_temp $zip_url $zip_url `
-                      -checksum $checksum -checksumType sha256 `
-                      -checksum64 $checksum -checksumType64 sha256
+Get-ChocolateyWebFile "$packageName" $zip_file_temp $zip_url `
+                      -checksum $checksum -checksumType $checksumType
 
 
 Write-Host ""
@@ -47,13 +45,5 @@ Write-Host "  * FROM: $zip_file_temp"
 Write-Host "  * TO..: $tempDir"
 Write-Host ""
 Get-ChocolateyUnzip "$zip_file_temp" "$tempDir"
-# param(
-# [string] $fileFullPath..: This is the full path to your zip file.
-# [string] $destination...: This is a directory where you would like the unzipped files to end up.
-# [string] $specificFolder: OPTIONAL - This is a specific directory within zip file to extract.
-# [string] $packageName...: OPTIONAL - This will faciliate logging unzip activity for subsequent uninstall
-#$scriptPath = (Split-Path -parent $MyInvocation.MyCommand.Definition)
-#Get-ChocolateyUnzip "c:\someFile.zip" $scriptPath somedirinzip\somedirinzip
-#Get-ChocolateyUnzip "C:\Chocolatey\pgadmin3\pgadmin3-{version}.zip" "C:\Chocolatey\pgadmin3.extracted"
 
 Install-ChocolateyPackage "$packageName" "$installerType" $silentArgs $msi_file_temp -validExitCodes $validExitCodes
