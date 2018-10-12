13728d04d46006fd48d79c2a055087073756f753
Chris Charabaruk <chris.charabaruk@outlook.com>
2018-10-11 20:03:59 -0400

    retroarch: Update to 1.7.5

--- retroarch/tools/chocolateyInstall.ps1
+++ retroarch/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿#NOTE: Please remove any commented lines to tidy up prior to releasing the package, including this one
 
 $packageName   = 'retroarch' # arbitrary name for the package, used in messages
-$url           = 'https://buildbot.libretro.com/stable/1.7.4/windows/x86/RetroArch.7z' # download url
-$url64         = 'https://buildbot.libretro.com/stable/1.7.4/windows/x86_64/RetroArch.7z' # 64bit URL here or remove - if installer decides, then use $url
-$checksum      = '7595a34f0bba5c2c3e4f9154e99839d4817a1b754e09f098d46eaeaeff226415'
-$checksum64    = 'e1817661a14443c075e6e333b8a9b6e9a018dfd77b7ec84e0fd386f19a142a2f'
+$url           = 'https://buildbot.libretro.com/stable/1.7.5/windows/x86/RetroArch.7z' # download url
+$url64         = 'https://buildbot.libretro.com/stable/1.7.5/windows/x86_64/RetroArch.7z' # 64bit URL here or remove - if installer decides, then use $url
+$checksum      = 'b28e874524e1074d20fca4ecad2d8710fd52920edfc21109e61320437ac6fa79'
+$checksum64    = 'aa71a32e6b6d05abfa095a931725c99d8301138206d2dd09fcf5bb1ca9fb5a32'
 $checksumType  = 'sha256'
 $checksumType64= 'sha256'
 
