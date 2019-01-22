4c214a73df6a28610bd77b43aa6e7e47b00adf91
Chris Charabaruk <chris.charabaruk@outlook.com>
2019-01-22 09:59:54 -0500

    pgadmin4: Update to 4.1

--- pgadmin4/tools/chocolateyInstall.ps1
+++ pgadmin4/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName = 'pgadmin4'
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url         = 'https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v4.0/windows/pgadmin4-4.0-x86.exe' # download url
-$checksum    = '993791c05d3a9af2fb67111c9fd8e8723ff2a7f0ead6ada033ec7b4dc18e5b38'
+$url         = 'https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v4.1/windows/pgadmin4-4.1-x86.exe' # download url
+$checksum    = '263f86206d29b983ab142121fa279b962f64c2d20f875811e725dff737c4585b'
 $checksumType= 'sha256'
 
 $packageArgs = @{
