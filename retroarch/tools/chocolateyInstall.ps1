5de6308cd9d6311f7dc7bf2d3813d870148123d1
Chris Charabaruk <chris.charabaruk@outlook.com>
2019-03-12 10:03:31 -0400

    retroarch: Update to 1.7.6

--- retroarch/tools/chocolateyInstall.ps1
+++ retroarch/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$packageName   = 'retroarch'
-$url           = 'https://buildbot.libretro.com/stable/1.7.5/windows/x86/RetroArch.7z'
-$url64         = 'https://buildbot.libretro.com/stable/1.7.5/windows/x86_64/RetroArch.7z'
-$checksum      = 'b28e874524e1074d20fca4ecad2d8710fd52920edfc21109e61320437ac6fa79'
-$checksum64    = 'aa71a32e6b6d05abfa095a931725c99d8301138206d2dd09fcf5bb1ca9fb5a32'
-$checksumType  = 'sha256'
-$checksumType64= 'sha256'
+$url           = 'https://buildbot.libretro.com/stable/1.7.6/windows/x86/RetroArch.7z'
+$url64         = 'https://buildbot.libretro.com/stable/1.7.6/windows/x86_64/RetroArch.7z'
+$checksum      = ''
+$checksum64    = ''
+$checksumType  = ''
+$checksumType64= ''
 
 
 # Get the package parameters and then back them up for the uninstaller
