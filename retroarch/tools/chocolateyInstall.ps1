edec89a9746ca446af9fbb702d1db1ea88a8d112
Chris Charabaruk <chris.charabaruk@outlook.com>
2020-05-07 21:45:48 -0400

    retroarch: Update to 1.8.6

--- retroarch/tools/chocolateyInstall.ps1
+++ retroarch/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName   = 'retroarch'
-$url           = 'https://buildbot.libretro.com/stable/1.8.5/windows/x86/RetroArch.7z'
-$url64         = 'https://buildbot.libretro.com/stable/1.8.5/windows/x86_64/RetroArch.7z'
-$checksum      = '9248e7e839e64c52a3c5a9d417b74225d691eb9b56c792f493cbc21263b033db'
-$checksum64    = 'bf463b00721829dc206e3f1da2293e22d144bbf4fca4185cd30e2156ce502ff8'
+$url           = 'https://buildbot.libretro.com/stable/1.8.6/windows/x86/RetroArch.7z'
+$url64         = 'https://buildbot.libretro.com/stable/1.8.6/windows/x86_64/RetroArch.7z'
+$checksum      = '24245d408509349dc430dd43dc802850d619d3e60301309a5b32e39cf5f17d33'
+$checksum64    = 'f300bf527a3d1a8af59828457313a24baa3099fa97e65f31af850ee6bd6878aa'
 $checksumType  = 'sha256'
 $checksumType64= 'sha256'
 
