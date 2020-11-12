dceaceaa3df9c330b40304b2996fe6a0ddcc49bf
Chris Charabaruk <chris.charabaruk@outlook.com>
2020-11-12 10:07:34 -0500

    pgadmin4: Update to 4.28

--- pgadmin4/tools/chocolateyInstall.ps1
+++ pgadmin4/tools/chocolateyInstall.ps1
@@ -2,10 +2,10 @@
 
 $packageName   = 'pgadmin4'
 $toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url           = 'https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v4.27/windows/pgadmin4-4.27-x86.exe' # download url
-$url64         = 'https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v4.27/windows/pgadmin4-4.27-x64.exe'
-$checksum      = '3906517eb0c1705762e8bcd5542dabd79c7bed9517065ca6a54d3e0eab31f548'
-$checksum64    = '44812137c946627e637b06f4c0d9dc43d9a54d7bdaa1c30e7330307d9b118abd'
+$url           = 'https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v4.28/windows/pgadmin4-4.28-x86.exe' # download url
+$url64         = 'https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v4.28/windows/pgadmin4-4.28-x64.exe'
+$checksum      = 'c33ce7070297d111c627f63fa27bd8e5d45c7335f788fbcaf15c5014eeb12b5f'
+$checksum64    = '88acc2307eb9875cedc6c85ce8b5e8680bebe30ce1a87b84235db532cd725782'
 $checksumType  = 'sha256'
 $checksumType64= 'sha256'
 
