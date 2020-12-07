b8401b2bf85c5bbe8712e200dca269a067bf0c96
Chris Charabaruk <chris.charabaruk@outlook.com>
2020-12-07 12:15:11 -0500

    fix(sandcastle): handle VSIX package changes
    The VSIX package for Sandcastle now requires Visual Studio 2017 or newer
    now, and the package file name has been changed regarding this. Since VS
    2015 support has been dropped, we can also simplify the code for
    installing the VSIX package.

--- sandcastle/tools/chocolateyInstall.ps1
+++ sandcastle/tools/chocolateyInstall.ps1
@@ -32,34 +32,29 @@ $packageArgs = @{
 Install-ChocolateyInstallPackage @packageArgs
 
 # Install-ChocolateyVsixPackage requires a URL, so build one out of the file path
-$vsix = Join-Path $zipDir "InstallResources\SHFBVisualStudioPackage_VS2015AndLater.vsix"
-$vsixUrl = "file:///" + $(Convert-Path $vsix).Replace("\", "/")
+$vsix = Join-Path $zipDir "InstallResources\SHFBVisualStudioPackage_VS2017AndLater.vsix"
 
 # Install-ChocolateyVsixPackage doesn't let us provide a list of supported versions for a package, unfortunately
 # Check for each version supported by the Sandcastle tools VSIX and call the function repeatedly as needed
-Get-VisualStudio | Where-Object { $_.installationVersion.Major -ge 14 } | ForEach-Object {
+Get-VisualStudio | Where-Object { $_.installationVersion.Major -ge 15 } | ForEach-Object {
   $vsver = $_.installationVersion.ToString(2)
   Write-Host "Installing VSIX package for Visual Studio $vsver"
 
-  if ( $_.installationVersion.Major -ge 15 ) {
-    $vssku = Split-Path $_.installationPath -Leaf
-    $vsixInstaller = Join-Path $_.installationPath 'Common7\IDE\VSIXInstaller.exe'
+  $vssku = Split-Path $_.installationPath -Leaf
+  $vsixInstaller = Join-Path $_.installationPath 'Common7\IDE\VSIXInstaller.exe'
 
-    # Otherwise VSIXInstaller.exe exits with error code 2003: NoApplicableSKUsException
-    if ( $_.installationVersion.Major -eq 15 ) { $vsver = '15.0' }
+  # Otherwise VSIXInstaller.exe exits with error code 2003: NoApplicableSKUsException
+  if ( $_.installationVersion.Major -eq 15 ) { $vsver = '15.0' }
 
-    Write-Host "    SKU is '$vssku'"
-    Write-Host "    Installation path is " $_.installationPath
+  Write-Host "    SKU is '$vssku'"
+  Write-Host "    Installation path is " $_.installationPath
 
-    $exitCode = Install-VsixEXT "$vsixInstaller" "$vsix" "$vsver" "$vssku"
-    if ($exitCode -eq 2004) { #2004: Blocking Process (need to close VS)
-      throw "A process is blocking the installation of the Sandcastle extension for " + $_.displayName + ". Please close all instances and try again."
-    }
-    if ($exitCode -gt 0 -and $exitCode -ne 1001) { #1001: Already installed
-      throw "There was an error installing the Sandcastle extension for " + $_.displayName + ". The exit code returned was $exitCode."
-    }
-  } elseif ( $_.installationVersion.Major -eq 14 ) {
-    Install-ChocolateyVsixPackage "$packageName" "$vsixUrl" -VsVersion 14
+  $exitCode = Install-VsixEXT "$vsixInstaller" "$vsix" "$vsver" "$vssku"
+  if ($exitCode -eq 2004) { #2004: Blocking Process (need to close VS)
+    throw "A process is blocking the installation of the Sandcastle extension for " + $_.displayName + ". Please close all instances and try again."
+  }
+  if ($exitCode -gt 0 -and $exitCode -ne 1001) { #1001: Already installed
+    throw "There was an error installing the Sandcastle extension for " + $_.displayName + ". The exit code returned was $exitCode."
   }
 }
 
