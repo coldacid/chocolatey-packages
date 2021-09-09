d8989b7b31d664ca3a6d00d89e69352a1cc4251b
Chris Charabaruk <chris.charabaruk@outlook.com>
2021-09-09 19:08:37 -0400

    pgadmin4: Update to 5.6

--- pgadmin4/tools/chocolateyInstall.ps1
+++ pgadmin4/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName   = 'pgadmin4'
 $toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64         = 'https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v5.6/windows/pgadmin4-5.6-x64.exe'
-$checksum64    = 'b4c383b518396699cd668d21b2464f03176e76ddcd14d2b6d0ade9950f25bc11'
+$url64         = 'https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v5.7/windows/pgadmin4-5.7-x64.exe'
+$checksum64    = 'edf0bab6031fc06d6b875cb1b76b06bb616d6431373726cc58775f769edb88b9'
 $checksumType64= 'sha256'
 
 $packageArgs = @{
