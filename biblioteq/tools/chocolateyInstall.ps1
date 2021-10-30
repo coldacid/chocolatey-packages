6be3203868521e3c807797fed72ea69a6ded820f
Chris Charabaruk <chris.charabaruk@outlook.com>
2021-10-30 02:02:28 -0400

    biblioteq: Update to 2021.10.30

--- biblioteq/tools/chocolateyInstall.ps1
+++ biblioteq/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName   = 'biblioteq' # arbitrary name for the package, used in messages
-$url           = 'https://github.com/textbrowser/biblioteq/releases/download/2021.09.10/BiblioteQ.zip' # download url
-$checksum      = 'ec325f151341050b0fa41caa94c3554ce5c01e666042c9b8e2d80cdaebce95af'
+$url           = 'https://github.com/textbrowser/biblioteq/releases/download/2021.10.30/BiblioteQ.zip' # download url
+$checksum      = '284bc9b6b68766ced372be3a5dd556db20b0f5d8514899bbd7c1f7f59277feed'
 $checksumType  = 'sha256'
 
 $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
